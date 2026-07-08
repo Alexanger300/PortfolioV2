038ea1188abdcd3a1f4e1fa42e6f5c622f397323
Alexandre Fatoux <Alexanger300>
2026-07-08 16:46:08 +0200

    Optimisation du code

--- project.js
+++ project.js
@@ -1,3 +1,5 @@
+import projectData from './projectData.js';
+
 const popup = document.getElementById("popup");
 const closeBtn = document.getElementById("close-btn");
 const popupTitle = document.getElementById("popup-title");
@@ -5,33 +7,6 @@ const popupDescription = document.getElementById("popup-description");
 const popupTechnologies = document.getElementById("popup-technologies");
 const popupLearned = document.getElementById("popup-learned");
 const popupLink = document.getElementById("popup-link");
-
-const projectData = {
-  red: {
-    title: 'Projet RED',
-    description: 'Un jeu créer en Golang tour par tour qui se déroule dans le terminal, je me suis occupé de la partie animations et du style.',
-    link: 'https://github.com/Alexanger300/projet-red_Arden',
-    technologies: 'Le jeu est codé en Golang.',
-    learned: 'Ce projet m\'a appris à développer en Golang et à créer une expérience utilisateur plus immersive dans un terminal.',
-    video: 'assets/videos/ProjetRED.mp4',
-  },
-  calysta: {
-    title: 'Calysta',
-    description: 'Calysta est un projet Ydays qui consiste à installer des salles de repos dans les lieux scolaires ; je me suis occupé du site web.',
-    link: 'https://github.com/Alexanger300/CalystaV2',
-    site: 'https://calysta-vitrine.vercel.app',
-    technologies: 'J\'ai codé le site en HTML, CSS et JavaScript.',
-    learned: 'Ce projet m\'a appris à créer un site web complet et à utiliser Vercel pour son déploiement.'
-  },
-  'groupie-tracker': {
-    title: 'Projet Groupie-Tracker',
-    description: 'Un site qui utilise une API pour retrouver des groupes musicaux ainsi que leurs informations et leurs concerts. Je me suis occupé du front-end du site web.',
-    link: 'https://github.com/333-m93/groupie-tracker',
-    technologies: 'Durant ce projet, j\'ai utilisé du HTML, CSS et du JavaScript.',
-    learned: 'Ce projet m\'a appris à créer un visuel attractif et une interface dynamique avec JavaScript.',
-    video: 'assets/videos/Présentation-Groupie-Tracker.mp4',
-  }
-};
 
 function openPopup(projectKey) {
   const project = projectData[projectKey];
---
+++ projectData.js
@@ -0,0 +1,27 @@
+const projectData = {
+  red: {
+    title: 'Projet RED',
+    description: 'Un jeu créer en Golang tour par tour qui se déroule dans le terminal, je me suis occupé de la partie animations et du style.',
+    link: 'https://github.com/Alexanger300/projet-red_Arden',
+    technologies: 'Le jeu est codé en Golang.',
+    learned: 'Ce projet m\'a appris à développer en Golang et à créer une expérience utilisateur plus immersive dans un terminal.',
+    video: 'assets/videos/ProjetRED.mp4',
+  },
+  calysta: {
+    title: 'Calysta',
+    description: 'Calysta est un projet Ydays qui consiste à installer des salles de repos dans les lieux scolaires ; je me suis occupé du site web.',
+    link: 'https://github.com/Alexanger300/CalystaV2',
+    site: 'https://calysta-vitrine.vercel.app',
+    technologies: 'J\'ai codé le site en HTML, CSS et JavaScript.',
+    learned: 'Ce projet m\'a appris à créer un site web complet et à utiliser Vercel pour son déploiement.'
+  },
+  'groupie-tracker': {
+    title: 'Projet Groupie-Tracker',
+    description: 'Un site qui utilise une API pour retrouver des groupes musicaux ainsi que leurs informations et leurs concerts. Je me suis occupé du front-end du site web.',
+    link: 'https://github.com/333-m93/groupie-tracker',
+    technologies: 'Durant ce projet, j\'ai utilisé du HTML, CSS et du JavaScript.',
+    learned: 'Ce projet m\'a appris à créer un visuel attractif et une interface dynamique avec JavaScript.',
+    video: 'assets/videos/Présentation-Groupie-Tracker.mp4',
+  }
+};
+export default projectData;
